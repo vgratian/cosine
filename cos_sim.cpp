0b34217ca602bc3363035081d788c4a9d4b3d02f
vgratian <vgratian@utopianlab.am>
2018-03-22 11:46:09 +0100

    first commit c++ version

---
+++ cos_sim.cpp
@@ -0,0 +1,45 @@
+
+/* Required libraries to use this code:
+#include <iostream>
+#include <math.h>
+*/
+
+class Cos_Sim {
+private:
+    int size;
+    float dot_prod;
+    float eucl_magn;
+    void get_dot_prod(float*, float*, int);
+    void get_eucl_magn(float*, float*, int);
+public:
+    float value;
+    Cos_Sim(float*, float*, int);
+};
+
+Cos_Sim::Cos_Sim(float *vect0, float *vect1, int size) {
+    // Calculate dot product
+    get_dot_prod(vect0, vect1, size);
+    // If dot product is 0 skip calculation
+    if (dot_prod == 0)
+        value = 0;
+    else {
+        get_eucl_magn(vect0, vect1, size);
+        value = dot_prod / eucl_magn;
+    }
+}
+
+void Cos_Sim::get_dot_prod(float *vect0, float *vect1, int size) {
+    dot_prod = 0.0;
+    for(int i=0; i<size; i++)
+        dot_prod += vect0[i] * vect1[i];
+}
+
+void Cos_Sim::get_eucl_magn(float *vect0, float *vect1, int size) {
+    float eucl_len0 = 0;
+    float eucl_len1 = 0;
+    for(int i=0; i<size; i++) {
+        eucl_len0 += pow(vect0[i], 2);
+        eucl_len1 += pow(vect1[i], 2);
+    }
+    eucl_magn = sqrt(eucl_len0) * sqrt(eucl_len1);
+}
