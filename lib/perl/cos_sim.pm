1232356bbbf3f2c59e2312931b71908916f4864f
Vachagan Gratian <vgratian@utopianlab.am>
2020-12-31 01:49:27 +0400

    perl to new design

--- lib/perl/cos_sim.pm
+++ lib/perl/cos_sim.pm
@@ -1,26 +1,19 @@
 #!/usr/bin/perl
 
 package cos_sim;
-use Math::Complex;
 
 sub get_cos_sim {
-    my @A = @{$_[0]};
-    my @B = @{$_[1]};
-
-    # Make sure vectors are of equal size
-    if (scalar @A != scalar @B) {
-        die "Vectors are not of equal size. Error";
-    }
-    my $size = scalar @A;
+    my ($size, $a, $b) = @_;
+    my $n = $size-1;
 
     # Calculate eucledian magnitude
-    my $A_len = 0;
-    my $B_len = 0;
-    foreach(0..$size) {
-        $A_len += $A[$_]**2;
-        $B_len += $B[$_]**2;
+    my $a_len = 0;
+    my $b_len = 0;
+    foreach(0..$n) {
+        $a_len += (@$a[$_]**2);
+        $b_len += (@$b[$_]**2);
     }
-    my $eucl_magn = sqrt($A_len * $B_len);
+    my $eucl_magn = sqrt($a_len * $b_len);
 
     # If 0, stop calculation
     if ($eucl_magn == 0) {
@@ -29,8 +22,8 @@ sub get_cos_sim {
 
     # Calculate dot product
     my $dot_prod = 0;
-    foreach(0..$size) {
-        $dot_prod += $A[$_] * $B[$_];
+    foreach(0..$n) {
+        $dot_prod += (@$a[$_] * @$b[$_]);
     }
 
     # Return cosine similarity
